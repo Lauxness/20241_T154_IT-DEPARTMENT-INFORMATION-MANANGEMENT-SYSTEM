2c57e2cafc72c645b924fbfbcb6c71e21d7c53bc
Rojo41 <2201104208@student.buksu.edu.ph>
2024-10-23 20:42:25 +0800

    Added Google Button

--- frontend/src/components/loginModal/LoginModal.jsx
+++ frontend/src/components/loginModal/LoginModal.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 import ReCAPTCHA from "react-google-recaptcha";
+import GoogleButton from "react-google-button";
 import "./style.css";
 
 function LoginModal(props) {
   return props.trigger ? (
     <>
       <div className="pop-up-container">
-        <div className="pop-up-inner">
-          <h4>Sign in with Google</h4>
+        <form className="pop-up-inner">
+          <h2>Sign in with Google</h2>
           <ReCAPTCHA sitekey="6Lf_0mkqAAAAAE1-c_dX0-Z0AXrkGZ6CDHy3jTRy" />
-        </div>
+          <GoogleButton
+            type="light"
+            onClick={() => {
+              console.log("Google button clicked");
+            }}
+          />
+        </form>
       </div>
     </>
   ) : (
