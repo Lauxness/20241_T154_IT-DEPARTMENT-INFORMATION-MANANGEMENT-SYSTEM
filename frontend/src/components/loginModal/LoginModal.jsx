3750654660bf8ab7184b4b4fadb799cb82456c8a
Rojo41 <2201104208@student.buksu.edu.ph>
2024-10-23 20:34:27 +0800

    Added Captcha

--- frontend/src/components/loginModal/LoginModal.jsx
+++ frontend/src/components/loginModal/LoginModal.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import ReCAPTCHA from "react-google-recaptcha";
 import "./style.css";
 
 function LoginModal(props) {
   return props.trigger ? (
     <>
       <div className="pop-up-container">
-        <div className="pop-up-inner"></div>
+        <div className="pop-up-inner">
+          <h4>Sign in with Google</h4>
+          <ReCAPTCHA sitekey="6Lf_0mkqAAAAAE1-c_dX0-Z0AXrkGZ6CDHy3jTRy" />
+        </div>
       </div>
     </>
   ) : (
