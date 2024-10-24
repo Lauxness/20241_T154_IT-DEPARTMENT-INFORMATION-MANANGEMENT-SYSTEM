5224e5a733a9a7ba5cbb749428b5a93fa7e352f8
Rojo41 <2201104208@student.buksu.edu.ph>
2024-10-24 21:04:13 +0800

    Updated Vite Dependecies

--- frontend/src/components/loginModal/LoginModal.jsx
+++ frontend/src/components/loginModal/LoginModal.jsx
@@ -1,22 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import ReCAPTCHA from "react-google-recaptcha";
 import GoogleButton from "react-google-button";
 import "./style.css";
 
 function LoginModal(props) {
+  const [captcha, setCaptcha] = useState("");
+
+  const updateCaptcha = (val) => {
+    setCaptcha(val);
+  };
+
+  const continueWithGoogle = () => {
+    const url = "http://localhost:3000/subscribe ";
+    fetch(url, {
+      method: "POST",
+      headers: {
+        accept: "application/json",
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({ captcha: captcha }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  };
   return props.trigger ? (
     <>
       <div className="pop-up-container">
-        <form className="pop-up-inner">
+        <div className="pop-up-inner">
           <h2>Sign in with Google</h2>
-          <ReCAPTCHA sitekey="6Lf_0mkqAAAAAE1-c_dX0-Z0AXrkGZ6CDHy3jTRy" />
-          <GoogleButton
-            type="light"
-            onClick={() => {
-              console.log("Google button clicked");
-            }}
+          <ReCAPTCHA
+            sitekey="6Lf_0mkqAAAAAE1-c_dX0-Z0AXrkGZ6CDHy3jTRy"
+            onChange={(val) => updateCaptcha(val)}
           />
-        </form>
+          <GoogleButton type="light" onClick={continueWithGoogle} />
+        </div>
       </div>
     </>
   ) : (
